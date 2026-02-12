a41a0c61bd527a3fcfd90bdd720621d921f26b64
sanjana-chetry <sanjanachetry118@gmail.com>
2026-02-12 23:36:52 +0530

    add drag-and-drop support with active and drag states

--- static/script.js
+++ static/script.js
@@ -1,8 +1,27 @@
 const file_inp=document.getElementById("resume-input");
 const subtitle=document.querySelector(".upload-subtitle")
+const uploadBox = document.querySelector(".upload-box");
 
 file_inp.addEventListener("change",function(){
     if(file_inp.files.length > 0){
         subtitle.textContent= file_inp.files[0].name + " selected";
+        uploadBox.classList.add("active");
+    }
+});
+uploadBox.addEventListener("dragover",function(e){
+    e.preventDefault();
+    uploadBox.classList.add("dragover");
+});
+uploadBox.addEventListener("dragleave",function(e){
+    e.preventDefault();
+    uploadBox.classList.remove("dragover");
+});
+uploadBox.addEventListener("drop",function(e){
+    e.preventDefault();
+    uploadBox.classList.remove("dragover");
+    if(e.dataTransfer.files.length >0){
+        file_inp.files=e.dataTransfer.files;
+        subtitle.textContent=file_inp.files[0].name + " selected";
+        uploadBox.classList.add("active");
     }
 });
